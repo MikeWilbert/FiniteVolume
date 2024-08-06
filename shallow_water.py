d34c581c540a9f5244f3aa296f4fa69df93111a4
Mike Wilbert <mike.wilbert@rub.de>
2024-08-06 18:13:30 +0200

    Shallow water: Rusanov does work now.

--- shallow_water.py
+++ shallow_water.py
@@ -11,7 +11,7 @@ def init():
   global x, Q, b 
   global b_start, h_0, g
 
-  A = 0.3 * h_0
+  A = 2. * h_0
 
   eta = np.zeros_like(x)
   b   = np.zeros_like(x)
@@ -26,7 +26,8 @@ def init():
   # b += - 0.8 * np.exp( - (x - 0.)**2 / 10. )
   # b += - 0.8 * np.exp( - (x + 10.)**2 / 10. )
   
-  eta += 1. * np.exp( - (x - 60.)**2 / 20. )
+  eta += 2. * np.exp( - (x - 80.)**2 / 20. )
+  # eta += A/h_0 * 1./np.cosh( np.sqrt(0.75 * A/h_0**3) * (x - 80.))
   
   H = b
   U += 0.
@@ -43,8 +44,6 @@ def init():
 def boundary(u):
 
   # outflow 
-  # u[0,0] = 0.
-  # u[0,1] = 0.
   u[0,0] = b[0]
   u[0,1] = b[1]
   u[0,-1] = b[-1]
@@ -54,13 +53,6 @@ def boundary(u):
   u[1,-1] = 0.
   u[1,-2] = 0.
 
-  # outflow 
-  # u[:,0] = u[:,2]
-  # u[:,1] = u[:,2]
-  
-  # u[:,-1] = u[:,-3]
-  # u[:,-2] = u[:,-3]
-
   # periodic
   # u[:,0] = u[:,-2]
   # u[:,1] = u[:,-3]
@@ -68,14 +60,16 @@ def boundary(u):
   # u[:,-1] = u[:,3]
   # u[:,-2] = u[:,2]
 
+def max_u(u):
+  
+  return np.abs( u[1,:] / ( u[0,:] + 1.e-12 ) ) + np.sqrt( u[0,:] * g )
+
 def calc_dt():
   
   global Q, g, dx, cfl
   
-  c = np.amax( np.abs( Q[1,:] / ( Q[0:] + 1.e-12 ) )  )
-  # c = np.amax( np.abs( Q[1,:] / ( Q[0:] + 1.e-12 ) ) + np.abs(np.sqrt( Q[0,:] * g ))  )
+  c = np.amax( max_u(Q) )
   
-  # return ( cfl * dx / 10. )
   return ( cfl * dx / c )
 
 def flux(u):
@@ -90,6 +84,7 @@ def flux(u):
   return f
 
 def dry(u):
+  
   u[:,:] = np.where( u[0,:] < 1.e-6, 0., u[:,:] )
 
 def step():
@@ -98,40 +93,31 @@ def step():
   
   dt = calc_dt()
   
-  # LxF
-  # Q[:,2:-2] = 0.5 * ( Q[:,3:-1] + Q[:,1:-3] ) - dt * ( flux( Q[:,3:-1] ) - flux( Q[:,1:-3] ) )
-  # Q[:,Ng:-Ng] = np.where( Q[0,Ng:-Ng] < 0.00001, 0., Q[:,Ng:-Ng] )
-  
-  # S = + ( b[3:-1] - b[1:-3]) / (2.*dx) * g * Q[0,2:-2]
-  
-  # Q[1,2:-2] += dt * S 
-  # Q[:,Ng:-Ng] = np.where( Q[0,Ng:-Ng] < 0.00001, 0., Q[:,Ng:-Ng] )
-  
-  # Rusanov  
-  
-  dry(Q)
   Q_l = Q[:,:-1]
   Q_r = Q[:,1:]
   
-  a = np.fmax( 
-              np.abs( Q_l[1,:] / ( Q_l[0:] + 1.e-12 ) ) + np.abs(np.sqrt( Q_l[0,:] * g )),
-              np.abs( Q_r[1,:] / ( Q_r[0:] + 1.e-12 ) ) + np.abs(np.sqrt( Q_r[0,:] * g ))
-              )
+  a = np.where( max_u(Q_l) > max_u(Q_r), max_u(Q_l), max_u(Q_r) )
   
+  # LxF
   # F = 0.5 * ( flux( Q_r ) + flux( Q_l ) - dx/dt * ( Q_r - Q_l ) )
+  # Rusanov  
   F = 0.5 * ( flux( Q_r ) + flux( Q_l ) - a * ( Q_r - Q_l ) )
+  
   F_L = F[:,1:-2]
   F_R = F[:,2:-1]
   
+  # Strang splitting
+  S = + ( b[3:-1] - b[1:-3]) / (2.*dx) * g * Q[0,2:-2]
+  Q[1,2:-2] += 0.5 * dt * S 
+  dry(Q)
   Q[:,Ng:-Ng] += - dt * ( F_R - F_L ) / dx
   dry(Q)
-  
   S = + ( b[3:-1] - b[1:-3]) / (2.*dx) * g * Q[0,2:-2]
-  
-  Q[1,2:-2] += dt * S 
+  Q[1,2:-2] += 0.5 * dt * S 
   dry(Q)
   
   boundary(Q)
+  dry(Q)
   
   t += dt
 
@@ -143,7 +129,7 @@ g = 9.81
 
 L = L_r + L_l
 N = 200
-cfl = 0.25
+cfl = 0.5
 
 Ng = 2
 
@@ -162,7 +148,7 @@ ax1.set_xlim(-L_l, L_r)
 ax1.set_ylim(-1.5*h_0, h_0)
 ax1.axhline(0.,-L_l, L_r, c='k', alpha=0.1, ls='--')
 
-# line_1, = ax1.plot(x, (Q[0,:]-h_0), c = 'k')
+# line_1, = ax1.plot(x, (Q[0,:]-h_0), c = 'b', alpha=0.5  )
 fill_1 = ax1.fill_between(x, (Q[0,:]-b), -b, facecolor = 'xkcd:bright blue')
 fill_2 = ax1.fill_between(x, -b, - 1.5 * h_0, facecolor = 'xkcd:light mustard')
 
